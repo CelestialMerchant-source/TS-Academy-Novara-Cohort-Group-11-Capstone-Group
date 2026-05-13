ef2d16c1e95198304aa60e7f578c1404ed7f393a
Timothyadeyemi2997 <timothyadeyemi758@gmail.com>
2026-05-13 08:56:02 +0100

    feat: Header, Logo with their styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef } from 'react';
+import Header from './components/Headers/Header';
+import PlanetCard from './components/PlanetCard';
+import PlanetsSection from './components/PlanetsSection';
+import DataTable from './components/DataTable';
+import ContactForm from './components/ContactForm';
+import Footer from './components/Footer';
+import './index.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const planetRef = useRef(null);
+  const formRef = useRef(null);
 
+  const scrollToPlanets = () => {
+    planetRef.current.scrollIntoView({ behavior: 'smooth' });
+  }
+  const scrollToForm = () => {
+    formRef.current.scrollIntoView({ behavior: 'smooth' });
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header/>
+    <div className='container'>
+      
+      <PlanetCard />
+      <PlanetsSection />
+      <DataTable />
+      <ContactForm />
+      <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
-
 export default App
