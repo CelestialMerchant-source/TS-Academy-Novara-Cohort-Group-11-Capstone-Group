f712b4bdb9b1870965de340038919192426112f4
Blessing-95 <blessingolaife7@gmail.com>
2026-05-17 13:56:32 +0100

    Added Hero Section Stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import DataTable from './components/DataTable'
-import './index.css'
+import HeroSection from "./components/HeroSection";
+import DataTable from "./components/DataTable";
+import "./index.css";
+import "./hero-section";
 
 function App() {
   return (
     <div>
+      <HeroSection />
       <DataTable />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
